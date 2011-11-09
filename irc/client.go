462eff6244d745701708c7a5b7884fbb5e44b616
Scott Lawrence <bytbox@gmail.com>
2011-11-09 15:59:33 -0500

    More comments

--- irc/client.go
+++ irc/client.go
@@ -173,31 +173,37 @@ func (c *Client) Listen() <-chan Message {
 	return ch
 }
 
-// Join the specified channel
+// Low-level method to join the specified channel. This does not modify the
+// Client's internal channel tracking, and so should not be used by most
+// clients.
 func (c *Client) Join(ch string) {
 	// TODO don't join until 001 is received
 	log.Print("JOIN ", ch)
 	fmt.Fprintf(c.out, "JOIN %s\n", ch)
 }
 
-// Join the specified channels
+// Join the specified channels. Equivalent to calling Join for each channel in the given slice.
 func (c *Client) JoinChannels(chs []string) {
 	for _, ch := range chs {
 		c.Join(ch)
 	}
 }
 
-// Leave the specified channel
+// Low-level method to leave the specified channel. This does not modify the
+// Client's internal channel tracking, and so should not be used by most
+// clients.
 func (c *Client) Part(ch string) {
 	log.Print("PART ", ch)
 	fmt.Fprintf(c.out, "PART %s\n", ch)
 }
 
+// Low-level method to send QUIT to the server.
 func (c *Client) Quit(msg string) {
 	log.Print("QUIT :", msg)
 	fmt.Fprintf(c.out, "QUIT :%s\n", msg)
 }
 
+// Low-level method to send a private message (PRIVMSG).
 func (c *Client) PrivMsg(to, msg string) {
 	fmt.Fprintf(c.out, "PRIVMSG %s :%s\n", to, msg)
 }
