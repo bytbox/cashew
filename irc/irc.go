094e62b27c014ca8cdca3a88570de09d22664988
Scott Lawrence <bytbox@gmail.com>
2011-11-09 15:45:04 -0500

    New util.go

--- irc/irc.go
+++ irc/irc.go
@@ -49,14 +49,6 @@ type Client struct {
 	out        io.Writer
 }
 
-func nextField(line string) (string, string, bool) {
-	fs := strings.SplitN(line, " ", 2)
-	if len(fs) > 1 {
-		return fs[0], fs[1], true
-	}
-	return fs[0], "", false
-}
-
 func parseServerMessage(line string) (m ServerMessage) {
 	m.Full = line
 	m.From, line, _ = nextField(line)
---
+++ irc/util.go
@@ -0,0 +1,9 @@
+package irc
+
+func nextField(line string) (string, string, bool) {
+	fs := strings.SplitN(line, " ", 2)
+	if len(fs) > 1 {
+		return fs[0], fs[1], true
+	}
+	return fs[0], "", false
+}
